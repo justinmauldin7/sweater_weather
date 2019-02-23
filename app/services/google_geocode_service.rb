a21416f5952cb493e934601e716b5474a9b0bc57
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-23 16:13:26 -0700

    Refactors Geocode service.

--- app/services/google_geocode_service.rb
+++ app/services/google_geocode_service.rb
@@ -17,15 +17,15 @@ class GoogleGeocodeService
 
   private
 
-  def get_json(path)
-    response = conn.get(path)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
   def conn
     Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
       faraday.params['key'] = ENV['GOOGLE_API_KEY']
       faraday.adapter  Faraday.default_adapter
     end
+  end
+
+  def get_json(path)
+    response = conn.get(path)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
