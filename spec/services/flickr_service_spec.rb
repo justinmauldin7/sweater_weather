a24f227fed8b17a315f4579589bd62a1573ccf7c
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-23 23:12:46 -0700

    Writes model test for Flickr Service.

---
+++ spec/services/flickr_service_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'with a city & state' do
+  it 'can send back a random photo in that city', :vcr do
+    city = 'denver,co'
+    farm = 8
+    server = "7888"
+    id = "40096180103"
+    secret = "266b9984d4"
+
+    photo = FlickrService.single_photo(city)
+
+    expect(photo[:farm]).to eq(farm)
+    expect(photo[:server]).to eq(server)
+    expect(photo[:id]).to eq(id)
+    expect(photo[:secret]).to eq(secret)
+  end
+end
