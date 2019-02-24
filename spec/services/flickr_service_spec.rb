18555acefe96f7c4d26c9e89b71a6f5f3e412858
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-23 23:46:39 -0700

    Adds model test for photo url creation from FlickrService, and adds filters for SimpleCov.

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -15,4 +15,17 @@ describe 'with a city & state' do
     expect(photo[:id]).to eq(id)
     expect(photo[:secret]).to eq(secret)
   end
+
+  it 'can create URL with photo that is retruned', :vcr do
+    city = 'denver,co'
+    farm = 8
+    server = "7888"
+    id = "40096180103"
+    secret = "266b9984d4"
+
+    FlickrService.single_photo(city)
+    url = FlickrService.photo_url
+
+    expect(url).to eq("https://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg")
+  end
 end
