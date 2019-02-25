fb7c2f723f33950196dad579455aa10cd71a03c9
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-25 10:14:21 -0700

    Updates Giphy request test with new assertions & keys.

--- spec/requests/api/v1/giphy_spec.rb
+++ spec/requests/api/v1/giphy_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe 'Giphy API' do
   it 'returns giphy related to the current weather', :vcr do
     city = 'denver,co'
-    time = "1541487600",
-    summary = "Mostly sunny in the morning.",
+    time = "1551078000"
+    summary = "Mostly cloudy until evening."
     url = "<GIPHY_URL_GOES_HERE>"
     days = 6
+    attribute_keys = ["copyright", "images"]
     image_keys = ["time", "summary", "url"]
 
     get "/api/v1/gifs?location=#{city}"
@@ -15,10 +16,11 @@ describe 'Giphy API' do
 
     giphy = JSON.parse(response.body)
 
-    expect(giphy["data"]["attributes"]["image"].count).to eq(days)
-    expect(giphy["data"]["attributes"]["image"][0].keys).to eq(image_keys)
-    expect(giphy["data"]["attributes"]["image"][0]["time"]).to eq(time)
-    expect(giphy["data"]["attributes"]["image"][0]["summary"]).to eq(summary)
-    expect(giphy["data"]["attributes"]["image"][0]["url"]).to eq(url)
+    expect(giphy["data"]["attributes"].keys).to eq(attribute_keys)
+    expect(giphy["data"]["attributes"]["images"].count).to eq(days)
+    expect(giphy["data"]["attributes"]["images"][0].keys).to eq(image_keys)
+    expect(giphy["data"]["attributes"]["images"][0]["time"]).to eq(time)
+    expect(giphy["data"]["attributes"]["images"][0]["summary"]).to eq(summary)
+    expect(giphy["data"]["attributes"]["images"][0]["url"]).to eq(url)
   end
 end
