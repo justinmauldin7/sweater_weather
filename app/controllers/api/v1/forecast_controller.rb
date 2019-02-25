fb54946bab7da63f2b2552cac30d1e526d2132d2
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-24 19:33:10 -0700

    Updates forecast controller to have current, daily & hourly forecast all in one PORO.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     weather_data = DarkSkyService.forecast(params[:location])
-    @current_forecast = CurrentForecast.new(weather_data, params[:location])
-    @hourly_forecast = HourlyForecast.new(weather_data, params[:location])
-    @daily_forecast = DailyForecast.new(weather_data, params[:location])
-    render json: CurrentForecastSerializer.new(@current_forecast)
-    render json: HourlyForecastSerializer.new(@hourly_forecast)
-    render json: DailyForecastSerializer.new(@daily_forecast)
+    @forecast = Forecast.new(weather_data, params[:location])
+    render json: ForecastSerializer.new(@forecast)
   end
 end
