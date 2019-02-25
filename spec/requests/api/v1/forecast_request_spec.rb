600d188b434275e11587ee1f6a833b92222a9eae
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-24 17:27:56 -0700

    Updates feature test for forecast.

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -7,5 +7,10 @@ describe 'Forecast API' do
     get "/api/v1/forecast?location=#{city}"
 
     expect(response).to be_successful
+
+    current_forecast = JSON.parse(response.body)
+    keys = ["city", "state", "date", "time", "icon", "temperature",
+            "feels_like", "humidity", "visibility", "uv_index", "summary"]
+    explect(current_forecast["data"]["attributes"].keys).to eq(keys)
   end
 end
