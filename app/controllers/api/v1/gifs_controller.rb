c0e62a7857cab8a06ee18a8d5b80184a5e0e20f0
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-25 09:27:09 -0700

    Adds needed content within index method.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::GifsController < ApplicationController
   def index
+    weather_data = DarkSkyService.forecast(params[:location])
+    @gifs = Gif.new(weather_data, params[:location])
+    render json: GifSerializer.new(@gifs)
   end
 end
