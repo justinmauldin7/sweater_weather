37cf34782d70413d42b98360aa9500cf3c61967b
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-24 17:28:37 -0700

    Updates forecast controller to break each part of the forecast into its own PORO & serializer.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    current_weather_data = DarkSkyService.forecast(params[:location])
-    forecast = Forecast.new(current_weather_data)
-    render json: ForecastSerializer.new(forecast)
+    weather_data = DarkSkyService.forecast(params[:location])
+    @current_forecast = CurrentForecast.new(weather_data, params[:location])
+    @hourly_forecast = HourlyForecast.new(weather_data, params[:location])
+    @daily_forecast = DailyForecast.new(weather_data, params[:location])
+    render json: CurrentForecastSerializer.new(@current_forecast)
+    render json: HourlyForecastSerializer.new(@hourly_forecast)
+    render json: DailyForecastSerializer.new(@daily_forecast)
   end
 end
