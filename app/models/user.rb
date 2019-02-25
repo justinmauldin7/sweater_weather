447983ce6b0ada60089cc41d08e3569d69bc0dc3
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-25 16:40:08 -0700

    Adds uniquness validation of email & validates presence of the api key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :password
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, :api_key
 
   has_secure_password
 end
