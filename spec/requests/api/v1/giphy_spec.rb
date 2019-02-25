19e64426f2379baa6d7842a1d19b493f42658b64
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-25 09:16:20 -0700

    Adds request test for returning giphy that corresponds to current weather.

---
+++ spec/requests/api/v1/giphy_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Giphy API' do
+  it 'returns giphy related to the current weather', :vcr do
+    city = 'denver,co'
+    time = "1541487600",
+    summary = "Mostly sunny in the morning.",
+    url = "<GIPHY_URL_GOES_HERE>"
+    giphy_count = 3
+    image_keys = ["city", "state"]
+
+    get "/api/v1/gifs?location=#{city}"
+
+    expect(response).to be_successful
+
+    giphy = JSON.parse(response.body)
+
+    expect(giphy["data"]["attributes"]["image"].count).to eq(giphy_count)
+    expect(giphy["data"]["attributes"]["image"][0].keys).to eq(image_keys)
+    expect(giphy["data"]["attributes"]["image"][0]["time"]).to eq(time)
+    expect(giphy["data"]["attributes"]["image"][0]["summary"]).to eq(summary)
+    expect(giphy["data"]["attributes"]["image"][0]["url"]).to eq(url)
+  end
+end
