5c6a11e1b0c9b54be571ec9000415025c41b8479
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 13:05:55 -0700

    Creates favorites table and migrates into the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_223741) do
+ActiveRecord::Schema.define(version: 2019_02_26_195245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "api_key"
   end
 
+  add_foreign_key "favorites", "users"
 end
