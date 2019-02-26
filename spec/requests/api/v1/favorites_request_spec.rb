b1e3b55d0c64a4c7b014f07d8fe2e3ea77fb4e45
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 14:39:59 -0700

    Updates favorite request test with updated expects.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,21 +5,21 @@ describe 'Favorites API' do
     email = "whatever@example.com"
     password = "password"
     city = "denver,co"
-    api_key = "2017e35f6a05da1db87865129db345"
+    api_key = "bfab8768f6a1932d68655e47699ce3"
     unauthorized_status = 401
-    location_response = "Denver, CO"
 
-    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
-    post "/api/v1/sessions?email=#{email}&password=#{password}"
+    user = User.create(email: email, password: password, password_confirmation: password, api_key: api_key)
+
+    post "/api/v1/sessions?email=#{user.email}&password=#{user.password}"
     post "/api/v1/favorites?location=#{city}&api_key=#{api_key}"
 
     expect(response).to be_successful
     expect(response.status).to_not eq(unauthorized_status)
 
-    user = JSON.parse(response.body)
+    favorite = JSON.parse(response.body)
 
-    expect(user['data']['attributes']['api_key'].length).to eq(api_key_length)
-    expect(user['data']['attributes']['location'].length).to eq(location_response)
+    expect(favorite['data']['attributes']['api_key']).to eq(api_key)
+    expect(favorite['data']['attributes']['location']).to eq(city)
   end
 
   it 'does not create favorite if password is bad', :vcr do
