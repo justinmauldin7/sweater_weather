6813656c9db2ee2bf1008d39eaee1332ac02c929
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-25 21:04:49 -0700

    Creates request model test for session api.

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
-describe 'Users API' do
-  it 'creates registered user with api_key', :vcr do
+describe 'Sessions API' do
+  it 'creates session for a registered user and returns api_key', :vcr do
     email = "whatever@example.com"
     password = "password"
-    created_status = 201
+    ok_status = 200
     unauthorized_status = 401
     api_key_length = 30
 
     post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
 
-    expect(response.status).to eq(created_status)
+    post "/api/v1/sessions?email=#{email}&password=#{password}"
+
+    expect(response.status).to eq(ok_status)
     expect(response.status).to_not eq(unauthorized_status)
 
     user = JSON.parse(response.body)
 
     expect(user['data']['attributes']['api_key'].length).to eq(api_key_length)
+
   end
 end
