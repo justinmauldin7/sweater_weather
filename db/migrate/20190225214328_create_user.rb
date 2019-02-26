f2b0370c9cb9741cabf8e355a3d520c5e3e60220
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 13:05:36 -0700

    Updates user migration to have created & updated at.

--- db/migrate/20190225214328_create_user.rb
+++ db/migrate/20190225214328_create_user.rb
@@ -1,8 +1,10 @@
-class User < ActiveRecord::Migration[5.2]
+class CreateUser < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email
       t.string :password_digest
+
+      t.timestamps
     end
   end
 end
