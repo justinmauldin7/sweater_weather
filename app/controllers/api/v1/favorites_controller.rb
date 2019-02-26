c9da6a9be9ee6fec0ce0ea17d22c9326e16884b0
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 14:37:17 -0700

    Updates expected index method in favorites controller to work.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      user.favorites.new(params[:location])
-      render json: FavoriteSerializer.new(user)
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      favorite = user.favorites.create(location: params[:location])
+      render json: FavoriteSerializer.new(favorite)
     else
       render json: {error: "401 Unauthorized"}, status: 401
     end
