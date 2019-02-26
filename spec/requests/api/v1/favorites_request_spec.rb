f2af2b972e557f3626683fcaad6071017ee99176
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 12:47:49 -0700

    Adds request test for favorites API.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -1,38 +1,40 @@
 require 'rails_helper'
 
-describe 'Sessions API' do
-  it 'creates session for a registered user and returns api_key', :vcr do
+describe 'Favorites API' do
+  it 'creates favorite for a registered user and returns api_key', :vcr do
     email = "whatever@example.com"
     password = "password"
-    ok_status = 200
+    city = "denver,co"
+    api_key = "2017e35f6a05da1db87865129db345"
     unauthorized_status = 401
-    api_key_length = 30
+    location_response = "Denver, CO"
 
     post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
-
     post "/api/v1/sessions?email=#{email}&password=#{password}"
+    post "/api/v1/favorites?location=#{city}&api_key=#{api_key}"
 
-    expect(response.status).to eq(ok_status)
+    expect(response).to be_successful
     expect(response.status).to_not eq(unauthorized_status)
 
     user = JSON.parse(response.body)
 
     expect(user['data']['attributes']['api_key'].length).to eq(api_key_length)
-
+    expect(user['data']['attributes']['location'].length).to eq(location_response)
   end
 
-  it 'does not create session if password is bad', :vcr do
+  it 'does not create favorite if password is bad', :vcr do
     email = "whatever@example.com"
     password = "password"
-    bad_password = "password111"
-    ok_status = 200
+    bad_password = "password1111"
+    city = "denver,co"
+    api_key = "2017e35f6a05da1db87865129db345"
     unauthorized_status = 401
 
     post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
-
     post "/api/v1/sessions?email=#{email}&password=#{bad_password}"
+    post "/api/v1/favorites?location=#{city}&api_key=#{api_key}"
 
     expect(response.status).to eq(unauthorized_status)
-    expect(response.status).to_not eq(ok_status)
+    expect(response).to_not be_successful
   end
 end
