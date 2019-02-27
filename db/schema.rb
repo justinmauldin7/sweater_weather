b07cdaf868690ff222df55ea4a6540397629dfa6
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-27 11:20:44 -0700

    Adds latitude & longitude fields to favorite model for caching the location.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_195245) do
+ActiveRecord::Schema.define(version: 2019_02_27_180553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_26_195245) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "latitude"
+    t.string "longitude"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
