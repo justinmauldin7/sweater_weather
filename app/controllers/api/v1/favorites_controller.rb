edd60b86ee33a39c98ae792b76ba3bee58f9ffa0
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-26 17:30:51 -0700

    Updates controller to not return serialized data on create & updates index assertions.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,8 +2,17 @@ class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
-      favorite = user.favorites.create(location: params[:location])
-      render json: FavoriteSerializer.new(favorite)
+      user.favorites.create(location: params[:location])
+      render json: {success: "200 OK"}, status: 200
+    else
+      render json: {error: "401 Unauthorized"}, status: 401
+    end
+  end
+
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    if user && user.favorites.count > 0
+      render json: FavoriteSerializer.new(user.favorites)
     else
       render json: {error: "401 Unauthorized"}, status: 401
     end
